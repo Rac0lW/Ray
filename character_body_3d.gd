7616d68b665998815d27f58da9d9bcd5d3b8ea67
Rac0lW <racolw@outlook.com>
2025-03-04 20:40:38 +0800

    Player could running now.

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -4,21 +4,42 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 9
 
+enum States{
+	Walking,
+	Running
+}
+
+var current_state := States.Walking
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Run"):
+		current_state = States.Running
+	
+	if event.is_action_released("Run"):
+		current_state = States.Walking
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 2
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction:Vector3 = (%CamPivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var current_speed = SPEED
+	
+	#Running or Walking
+	if current_state == States.Running:
+		current_speed = SPEED * 2
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		current_speed = SPEED
+	
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
