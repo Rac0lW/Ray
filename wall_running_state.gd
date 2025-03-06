5d8969b4af7eda70b368573678eae79419734e8e
Rac0lW <racolw@outlook.com>
2025-03-06 20:24:34 +0800

    fix the bugs of the wall running direction.

--- wall_running_state.gd
+++ wall_running_state.gd
@@ -2,8 +2,15 @@ extends State
 @onready var player: CharacterBody3D = %Player
 
 const WALL_RUNNING_SPEED = 10.0
+const JUMP_VELOCITY = 5.0
 
-	
+@onready var ray_cast_2_left: RayCast3D = %RayCast2Left
+@onready var ray_cast_2_right: RayCast3D = %RayCast2Right
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		pass
+		
 
 func _physics_process(_delta: float) -> void:
 	
@@ -14,16 +21,16 @@ func _physics_process(_delta: float) -> void:
 		#TODO：需要RayCast在更加合理的长度
 		switch_state.emit(%BaseMoveState)
 	
+	
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	#TODO: 在转向其他方向时后依然能全速运行
 	var direction:Vector3 = (%CamPivot.transform.basis * Vector3(0, 0, input_dir.y)).normalized()
 	var current_speed = WALL_RUNNING_SPEED
 	if direction:
-		player.velocity.x = direction.x * current_speed
-		#应急处理方式， 解决非预期的速度残留
+		player.velocity.z = direction.z * current_speed
 		player.velocity.y = 0
 	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, current_speed)
+		player.velocity.z = move_toward(player.velocity.z, 0, current_speed)
 		player.velocity.y = 0
 
 	player.move_and_slide()
