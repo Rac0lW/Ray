3a32f6629c2f8a550f303ae0f78414c8293ff2a2
Rac0lW <racolw@outlook.com>
2025-03-06 19:51:47 +0800

    Adjust the raycasts' transform y, and add State Crouch.

--- crouching_state.gd
+++ crouching_state.gd
@@ -1,31 +1,23 @@
 extends State
 
+
 @onready var player: CharacterBody3D = %Player
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 9
-
-enum States{
-	Walking,
-	Running
-}
-var current_state := States.Walking
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+const SPEED = 3.0
 
 func enter():
 	active()
-	#解决WallRunning状态所中断的跑走切换状态
-	current_state = States.Walking
+	animation_player.play("Crouch")
+	
+func exit():
+	inactive()
+	animation_player.play_backwards("Crouch")
+	
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Run"):
-		current_state = States.Running
-	
-	if event.is_action_released("Run"):
-		current_state = States.Walking
-		
-	if event.is_action_pressed("Crouch"):
-		switch_state.emit(%CrouchingState)
-	
+	if event.is_action_released("Crouch"):
+		switch_state.emit(%BaseMoveState)
+
 func _physics_process(delta: float) -> void:
 	
 	if player.is_on_wall_only() and (%RayCast2Right.is_colliding() or %RayCast2Left.is_colliding()):
@@ -34,17 +26,9 @@ func _physics_process(delta: float) -> void:
 	if not player.is_on_floor():
 		player.velocity += player.get_gravity() * delta * 2
 
-	if Input.is_action_just_pressed("ui_accept"):
-		player.velocity.y = JUMP_VELOCITY
-
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction:Vector3 = (%CamPivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	var current_speed = SPEED
-	
-	if current_state == States.Running:
-		current_speed = SPEED * 2
-	else:
-		current_speed = SPEED
 	
 	if direction:
 		#TODO: 增加一些平滑度， 比如使用move_toward，使用双倍的current_speed作为加速参数
